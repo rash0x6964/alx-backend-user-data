6dd98fee0e869dd9b0e72e5c4455f521f1933275
rash0x6964 <rash0x6964@gmail.com>
2024-09-02 15:44:42 +0100

    Basic authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""Authentication module.
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Authentication class.
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Method to check if auth is required.
+        """
+        if path is None:
+            return True
+
+        if not excluded_paths:
+            return True
+
+        if not path.endswith('/'):
+            path += '/'
+
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith('*'):
+                if path.startswith(excluded_path[:-1]):
+                    return False
+            elif excluded_path == path:
+                return False
+
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """ Method to get authorization header.
+        """
+        if request is not None:
+            return request.headers.get('Authorization', None)
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Method to get user from request.
+        """
+        return None
