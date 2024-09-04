119a5886165fe36571314de0abd1897551162933
rash0x6964 <rash0x6964@gmail.com>
2024-09-04 15:05:35 +0100

    task 5: Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -55,12 +55,14 @@ def authenticate_user():
         '/api/v1/status/',
         '/api/v1/unauthorized/',
         '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
     ]
     if auth.require_auth(request.path, excluded_paths):
         auth_header = auth.authorization_header(request)
+        cookie = auth.session_cookie(request)
         user = auth.current_user(request)
         request.current_user = user
-        if auth_header is None:
+        if auth_header is None and cookie is None:
             abort(401)
         if user is None:
             abort(403)
