161535b77ade211f0fb512a90048787181aee24c
rash0x6964 <rash0x6964@gmail.com>
2024-09-11 13:44:41 +0100

    task 1: create user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,11 +1,14 @@
+#!/usr/bin/env python3
 """DB module
 """
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 
 from user import Base, User
+
 
 class DB:
     """DB class
