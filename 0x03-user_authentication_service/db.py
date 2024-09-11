408bdcb362a48426af37ad53df6c48404ab41b06
rash0x6964 <rash0x6964@gmail.com>
2024-09-11 15:33:03 +0100

    task 2: Find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -46,12 +46,10 @@ class DB:
     def find_user_by(self, **kwargs) -> User:
         """Finds a user based on arbitrary keyword arguments.
         """
-        user_keys = ['id', 'email', 'hashed_password', 'session_id',
-                     'reset_token']
-        for key in kwargs.keys():
-            if key not in user_keys:
-                raise InvalidRequestError
-        result = self._session.query(User).filter_by(**kwargs).first()
-        if result is None:
-            raise NoResultFound
-        return result
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+            if not user:
+                raise NoResultFound
+            return user
+        except AttributeError:
+            raise InvalidRequestError
