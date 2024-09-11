929e56a9f397f48ed46b5dc561b37e9e72f7a920
rash0x6964 <rash0x6964@gmail.com>
2024-09-11 15:51:19 +0100

    task 5: Register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -22,12 +22,12 @@ class Auth:
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
-            """Registers a new user with the given email and password.
-            """
-            try:
-                self._db.find_user_by(email=email)
-                raise ValueError(f"User {email} already exists")
-            except NoResultFound:
-                hashed_password = _hash_password(password)
-                user = self._db.add_user(email, hashed_password)
-                return user
+        """Registers a new user with the given email and password.
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            user = self._db.add_user(email, hashed_password)
+            return user
