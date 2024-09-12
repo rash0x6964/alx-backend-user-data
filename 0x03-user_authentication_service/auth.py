c4d6054f224308cf0388d16fccdf1809d093d2ef
rash0x6964 <rash0x6964@gmail.com>
2024-09-13 00:08:57 +0100

    add docs

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -27,6 +27,8 @@ class Auth:
     """
 
     def __init__(self):
+        """Initializes DB
+        """
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
@@ -34,7 +36,7 @@ class Auth:
         """
         try:
             self._db.find_user_by(email=email)
-            raise ValueError(f"User {email} already exists.")
+            raise ValueError(f"User {email} already exists")
         except NoResultFound:
             hashed_password = _hash_password(password)
             user = self._db.add_user(email, hashed_password)
