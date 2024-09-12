583909d61a010bc6dd90eb52c54fecd888fa1769
rash0x6964 <rash0x6964@gmail.com>
2024-09-13 00:07:02 +0100

    ...

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -34,7 +34,7 @@ class Auth:
         """
         try:
             self._db.find_user_by(email=email)
-            raise ValueError(f"User {email} already exists")
+            raise ValueError("User {} already exists".format(email))
         except NoResultFound:
             hashed_password = _hash_password(password)
             user = self._db.add_user(email, hashed_password)
