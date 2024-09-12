46dcfbcab1941924317cf5d7f289ad33645edca2
rash0x6964 <rash0x6964@gmail.com>
2024-09-13 00:20:59 +0100

    ...

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -38,9 +38,11 @@ class Auth:
             self._db.find_user_by(email=email)
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
-            hashed_password = _hash_password(password)
-            user = self._db.add_user(email, hashed_password)
-            return user
+            pass
+        
+        hashed_password = _hash_password(password)
+        user = self._db.add_user(email, hashed_password)
+        return user
 
     def valid_login(self, email: str, password: str) -> bool:
         """Expect email and password required arguments
